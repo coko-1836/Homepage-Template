e99d1872320437a6187679ab9a09250ca4fbd71a
coko-go <89577404+coko-go@users.noreply.github.com>
2021-09-23 12:42:31 +0900

    Delete blog1.php

--- blog1.php
+++
@@ -1,9 +0,0 @@
-<?php $title = 'テキストエディタについて';
-$time = '2021.09.14';
-$writer = 'coko-go';
-$eyecatch = 'img/blog-thumb0.png';
- include('php/config.php');
- include('php/head.php') ; 
- include('php/header.php'); 
- include('php/top-main.php'); 
- include('php/footer.php'); ?>
